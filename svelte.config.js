7e36933f65d1bcf579785fd01fcaad0738459fd3
Gabriel Town <rieltown@gmail.com>
2022-09-08 11:43:04 -0600

    added adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+		}),
+	},
+}
