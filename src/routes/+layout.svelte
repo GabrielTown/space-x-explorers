d41a4925a1da4744c3d57b99ec115fd213ad1037
Gabriel Town <rieltown@gmail.com>
2022-09-08 13:15:16 -0600

    added about and simple navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">SpaceX Exploreres</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</nav>
+
+<slot></slot>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About | SpaceX Explorers</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>Built with SvelteKit</p>
