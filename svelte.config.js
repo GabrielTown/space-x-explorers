080a9b6dbf51232a3d1161eba2e2f6dac75bbdef
Gabriel Town <rieltown@gmail.com>
2022-09-12 15:21:44 -0600

    configure for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ export default {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
+			crawl: true,
+			enabled: true,
+			onError: 'continue',
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
