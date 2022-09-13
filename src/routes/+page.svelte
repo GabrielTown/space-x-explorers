f92c4440c247b859d9cf9a6e7fb101f64459d0d2
Gabriel Town <rieltown@gmail.com>
2022-09-13 10:23:10 -0600

    edited title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,8 @@
     <title>About | SpaceX Explorers</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>SpaceX Explorers</h1>
+<p>Launching your knowledge of rocket launches in to outer space!</p>
 
 <ul>
     {#each data.allLaunches.launches as {slug, name, date}}
--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,11 @@ export default {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			crawl: true,
-			enabled: true,
-			onError: 'continue',
+			prerender: {
+				crawl: true,
+				enabled: true,
+				onError: 'continue',
+			},
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
